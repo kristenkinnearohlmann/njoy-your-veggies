7cafe349810b05f7925a5295c37466386d680c01
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-01 18:28:59 -0500

    Added memoized current_user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     before_action :authenticated_user
 
     def current_user
-        User.find_by(id: session[:user_id]) if session[:user_id]
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
