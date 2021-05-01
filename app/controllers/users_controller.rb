86065bb3b2170fa5c5880b74e656fe2ba9c16028
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-01 08:47:45 -0500

    Drafted users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,33 @@
 class UsersController < ApplicationController
+    skip_before_action :authenticated_user, only: [:new, :create]
 
     def index
-
+        # TODO: Scope method in User to return only users
+        # that have contributed recipes
+        @user = User.all
     end
 
     def show
-
+        @user = current_user
     end
 
     def new
-
+        @user = User.new
     end
 
     def create
-
+        if user = User.create(user_params)
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            render new_user_path
+        end
     end
 
     private
 
     def user_params
-
+        params.require(:user).permit(:email, :password, :name)
     end
-    
+
 end
