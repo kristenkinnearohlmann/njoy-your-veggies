495c956e5fb3e53e5ef7ce6b6608e247eb881005
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-01 14:42:51 -0500

    Added uid and provider to users, completed implementation of FB login, need to finish local login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,40 +6,28 @@ class SessionsController < ApplicationController
     end
 
     def create
-        byebug
         if params.has_key?(:provider)
             create_fb if params[:provider] == "facebook"
         else
             create_local
         end
-        # user = User.find_by(email: params[:email])
-        # if user && user.authenticate(params[:password])
-        #     login_success(user)
-        #     # session[:user_id] = user.id
-        #     # redirect_to user_path(user)
-        # else
-        #     redirect_to login_path
-        # end
     end
 
     def create_local
-        byebug
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
             login_success(user)
-            # session[:user_id] = user.id
-            # redirect_to user_path(user)
         else
             redirect_to login_path
         end
     end
 
     def create_fb
-        byebug
-        user = User.find_or_create_by(id: auth['uid']) do |u|
+        user = User.find_or_create_by(uid: auth['uid']) do |u|
             u.email = auth['info']['email']
             u.password = SecureRandom.hex
             u.name = auth['info']['name']
+            u.provider = params[:provider]
         end
 
         login_success(user)
@@ -52,16 +40,11 @@ class SessionsController < ApplicationController
 
     private
 
-    def session_params
-
-    end
-
     def auth
         request.env['omniauth.auth']
     end
 
     def login_success(user)
-        byebug
         session[:user_id] = user.id
         redirect_to user_path(user)
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_034604) do
+ActiveRecord::Schema.define(version: 2021_05_01_193518) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_05_01_034604) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "uid"
+    t.string "provider"
   end
 
 end
