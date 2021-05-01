a2195711e63d83f15ac7490922d8aeba18c5955d
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-01 09:04:38 -0500

    Added authenticated user logic to static controller and restored to other controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
-    # before_action :authenticated_user
+    before_action :authenticated_user
 
     def current_user
         User.find_by(id: session[:user_id]) if session[:user_id]
@@ -11,8 +11,9 @@ class ApplicationController < ActionController::Base
         !!current_user
     end
 
-    # def authenticated_user
+    def authenticated_user
+        byebug
     #     redirect_to root_path unless logged_in?
-    # end
-    
+    end
+
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,5 @@
 class StaticController < ApplicationController
+    skip_before_action :authenticated_user, only: [:home, :about]
 
     def home
         
