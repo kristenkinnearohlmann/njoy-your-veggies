a3018d2b94864c72a73a1f3190575ff7a5a736b9
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-01 13:45:47 -0500

    Finished styling form errors, added Omniauth gems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-    def form_errors(form_obj)
-        form_obj.errors.full_messages
+    def form_errors(form_obj, attribute = nil)
+        attribute == nil ? form_obj.errors.full_messages : form_obj.errors.full_messages_for(attribute).first
     end
 
 end
