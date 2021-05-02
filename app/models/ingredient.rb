ab3ab15ba21f171bd5abb36c01dba4367b8a55f8
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-02 10:32:06 -0500

    Added ingredient method to make the singular version of the name

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,10 @@ class Ingredient < ApplicationRecord
     validates :name, presence: true
     validate :name_singular?
 
+    def name_to_singular
+        name = name.singularize
+    end
+
     private
 
     def name_singular?
