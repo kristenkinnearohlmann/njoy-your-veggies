13c47aee8355f03dfecab36a2643dc53248b5369
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-02 20:39:21 -0500

    Resolved user time period method

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,22 +16,29 @@ class User < ApplicationRecord
 
         days = (start_date...current_date).count
         months = (current_date.year * 12 + current_date.month) - (start_date.year * 12 + start_date.month)
-        byebug
+
         user_years_fmt(days, months % 12, months / 12) 
     end
 
     def user_years_fmt(days, months, years)
-        byebug
 
-        year_text = ""
-        month_text = ""
-        day_text = ""
+        timeframe = []
+        timeframe << date_part_pluralize(years,'year')
+        timeframe << date_part_pluralize(months,'month')
+        timeframe << date_part_pluralize(days,'day')
+        timeframe.collect {|part| part if part != ""}.compact.join(", ")
 
-        if years != 0
-            year_text = "#{years} #{"year".pluralize} and "
+    end
+
+    def date_part_pluralize(amount, date_part)
+
+        if amount == 0
+            date_part_text = ""
+        else
+            date_part_text = "#{amount} #{date_part}"
+            "#{date_part_text}s" if amount >= 1
         end
-
-        "#{year_text}#{"month".pluralize}"
+        
     end
 
 end
