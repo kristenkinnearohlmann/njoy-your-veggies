3127fd10e8e920fcdebe0e40eb7b9d6d08b1a282
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-02 22:14:21 -0500

    Refactored the recipe listing into a partial and a helper

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,6 +1,7 @@
 module RecipesHelper
-    def recipe_detail(recipe)
-        content_tag(:p, link_to(recipe.name,recipe_path(recipe.id)))
-        contnet_tag(:p, recipe.description)
+    def p_for_recipe(recipe)
+        content_tag(:p, recipe) do
+            yield
+        end
     end
 end
