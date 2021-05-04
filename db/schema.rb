a803587d7c276eb8a296692735dc37553afeee4d
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-03 21:42:38 -0500

    Added image column to recipe, updated show to display related image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_195654) do
+ActiveRecord::Schema.define(version: 2021_05_04_023836) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_05_02_195654) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "image"
   end
 
   create_table "users", force: :cascade do |t|
